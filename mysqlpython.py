05529d09f4b6d190ce26736052483330fb4afa21
锋锋 <449003613@qq.com>
2020-01-08 15:56:03 +0800

    Initial commit

--- mysqlpython.py
+++ mysqlpython.py
@@ -1,48 +1,4 @@
-import csv
-import MySQLdb
-import sys
-import pandas as pd
-from datetime import datetime, date
-# input_file = "data/supplier_data.csv"
-# # Connect to a MySQL database
-# con = MySQLdb.connect(host='127.0.0.1',
-# 					  port=3306,
-# 					  db='my_suppliers',
-# 					  user='root',
-# 					  passwd='83610361',
-# 					  charset='utf8' #编码方式
-# 					 )
-# c = con.cursor()
-# file_reader = csv.reader(open(input_file, 'r'), delimiter=',')
-# header = next(file_reader)
-#
-# #将csv数据写入数据库
-# for row in file_reader:
-# 	data = []
-# 	for column_index in range(len(header)):
-# 		if column_index < 4:
-# 			data.append(str(row[column_index]).lstrip('$')\
-# 			.replace(',', '').strip())
-# 		else:
-# 			a_date = datetime.date(datetime.strptime(\
-# 			str(row[column_index]), '%m/%d/%y'))
-# 			# %Y: year is 2016; %y: year is 15
-# 			a_date = a_date.strftime('%Y-%m-%d')
-# 			data.append(a_date)
-# 	print(data)
-# 	c.execute("""INSERT INTO Suppliers VALUES (%s, %s, %s, %s, %s);""", data)
-# con.commit() #执行以上操作
-# #查询表里面数据
-# c.execute("SELECT * FROM Suppliers")
-# rows = c.fetchall()
-# for row in rows:
-# 	row_list_output = []
-# 	for column_index in range(len(row)):
-# 		row_list_output.append(str(row[column_index]))
-# 	print(row_list_output)
-# #最后应该关闭所有的游标和连接
-# c.close()
-# con.close()
+
 #查询perhourdata数据库里面数据
 con = MySQLdb.connect(host='127.0.0.1',
 					  port=3306,
@@ -52,7 +8,8 @@ con = MySQLdb.connect(host='127.0.0.1',
 					  charset='utf8'  # 编码方式
 					  )
 c = con.cursor()
-c.execute("SELECT * FROM hour_data00to08 where 区站号='58724'")
+#c.execute("SELECT * FROM hour_data00to08 where 区站号='58724'")
+c.execute("select * from hour_data09to18 where 区站号='F3101'")
 rows = c.fetchall()
 col = c.description
 colname=[]
@@ -60,4 +17,4 @@ colname=[]
 for i in range(len(col)):
 	colname.append(col[i][0])
 test=pd.DataFrame(columns=colname,data=rows)
-test.to_csv('data/58724.csv',index=False)
+test.to_csv('data/F3101.csv',index=False)
